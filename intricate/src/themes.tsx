830e55539a2395622073200b53cf0714d857de38
Vanessa Munson <vanessa.munson03@gmail.com>
2025-01-21 20:15:58 +0100

    Define theme names and colors

---
+++ intricate/src/themes.tsx
@@ -0,0 +1,75 @@
+export interface themeColors {
+    primary: string;
+    secondary: string;
+    tertiary: string;
+    pale: string;
+    dark: string;
+    accent: string;
+};
+
+export const themes = {
+    forest: {
+        primary: '#479340',
+        secondary: '#9FB49D',
+        tertiary: '8F9340',
+        pale: '#DEF7DC',
+        dark: '#275323',
+        accent: '#CB5F51',
+    },
+    magic: {
+        primary: '#934074',
+        secondary: '#A6A0A4',
+        tertiary: '#934051',
+        pale: '#F5E2EE',
+        dark: '#6E3057',
+        accent: '#D7B83E',
+    },
+    coral: {
+        primary: '#F47865',
+        secondary: '#BA9C90',
+        tertiary: '#F48D65',
+        pale: '#FDE3D8',
+        dark: '#865F50',
+        accent: '#55B1D9',
+    },
+    coast: {
+        primary: '#408FE9',
+        secondary: '#A8B0B9',
+        tertiary: '#40DBE9',
+        pale: '#D9E7F8',
+        dark: '#245082',
+        accent: '#ED9F45',
+    },
+    rose: {
+        primary: '#F992AF',
+        secondary: '#CBB4BA',
+        tertiary: '#F9A392',
+        pale: '#FEE2EA',
+        dark: '#6C3D4A',
+        accent: '#5FAF7A',
+    },
+    brave: {
+        primary: '#D73336',
+        secondary: '#BE9899',
+        tertiary: '#E23F26',
+        pale: '#FDD6D6',
+        dark: '#741A1B',
+        accent: '#FDCC2A',
+    },
+    seaside: {
+        primary: '#20D0AD',
+        secondary: '#A8C9C2',
+        tertiary: '#20C2D0',
+        pale: '#E6FBF7',
+        dark: '#0D5345',
+        accent: '#C01A1D',
+    },
+    herbal: {
+        primary: '#22E560',
+        secondary: '#BFC9A8',
+        tertiary: '#AAE522',
+        pale: '#C9FFDA',
+        dark: '#0B5221',
+        accent: '#C01A5A',
+    },
+};
